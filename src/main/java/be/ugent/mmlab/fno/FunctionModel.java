e644e70bc535dc999e11ff112a8f60453837ec4a
Ben De Meester <ben.demeester@ugent.be>
2016-12-13 17:33:28 +0100

    first version

---
+++ src/main/java/be/ugent/mmlab/fno/FunctionModel.java
@@ -0,0 +1,50 @@
+package be.ugent.mmlab.fno;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+/**
+ * Function Model
+ *
+ * @author bjdmeest
+ */
+public class FunctionModel {
+
+    private final String[] parameters;
+    private String URI;
+    private Method method;
+
+    public FunctionModel(String URI, Method m, String[] parameters) {
+        this.URI = URI;
+        this.method = m;
+        this.parameters = parameters;
+    }
+
+    public Object execute(Map<String, String> args) {
+        Object[] parameters = this.getParameters(args);
+        try {
+            return this.method.invoke(null, parameters);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            // Nothing to do?
+            e.printStackTrace(); // maybe this? :p
+        }
+        return null;
+    }
+
+    public String getURI() {
+        return URI;
+    }
+
+    private Object[] getParameters(Map<String, String> parameters) {
+        Object[] args = new Object[this.parameters.length];
+        for (int i = 0; i < this.parameters.length; i++) {
+            if(parameters.get(this.parameters[i]) != null) {
+                args[i] = parameters.get(this.parameters[i]);
+            } else {
+                args[i] = null;
+            }
+        }
+        return args;
+    }
+}
